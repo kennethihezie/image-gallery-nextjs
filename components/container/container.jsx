49db78455a2a048a5bc2e9aed26e2e693947fedc
collins ihezie <collinsihezie6@gmail.com>
2022-09-30 06:58:33 +0100

    initial  commit

---
+++ components/container/container.jsx
@@ -0,0 +1,97 @@
+const Container = () =>  {
+    return (
+        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div className="relative group">
+                <img src="/images/image1.jpg" className="w-72" alt="" />
+                <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+                    <div className="flex justify-between w-full">
+                        <div className="font-normal">
+                            <p className="text-sm">Abstract Painting</p>
+                            <p className="text-xs">245 likes - 35 shares</p>
+                        </div>
+                        <div className="flex items-center">
+                                <img src="/images/bookmark.svg" alt="boolmark" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative group">
+                <img src="/images/image2.jpg" className="w-72" alt="" />
+                <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+                    <div className="flex justify-between w-full">
+                        <div className="font-normal">
+                            <p className="text-sm">Abstract Painting</p>
+                            <p className="text-xs">245 likes - 35 shares</p>
+                        </div>
+                        <div className="flex items-center">
+                                <img src="/images/bookmark.svg" alt="boolmark" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative group">
+                <img src="/images/image3.jpg" className="w-72" alt="" />
+                <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+                    <div className="flex justify-between w-full">
+                        <div className="font-normal">
+                            <p className="text-sm">Abstract Painting</p>
+                            <p className="text-xs">245 likes - 35 shares</p>
+                        </div>
+                        <div className="flex items-center">
+                                <img src="/images/bookmark.svg" alt="boolmark" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative group">
+                <img src="/images/image4.jpg" className="w-72" alt="" />
+                <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+                    <div className="flex justify-between w-full">
+                        <div className="font-normal">
+                            <p className="text-sm">Abstract Painting</p>
+                            <p className="text-xs">245 likes - 35 shares</p>
+                        </div>
+                        <div className="flex items-center">
+                                <img src="/images/bookmark.svg" alt="boolmark" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative group">
+                <img src="/images/image5.jpg" className="w-72" alt="" />
+                <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+                    <div className="flex justify-between w-full">
+                        <div className="font-normal">
+                            <p className="text-sm">Abstract Painting</p>
+                            <p className="text-xs">245 likes - 35 shares</p>
+                        </div>
+                        <div className="flex items-center">
+                                <img src="/images/bookmark.svg" alt="boolmark" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="relative group">
+                <img src="/images/image6.jpg" className="w-72" alt="" />
+                <div className="absolute bottom-0 left-0 right-0 p-2 px-4 text-white duration-500 bg-black opacity-0 group-hover:opacity-100 bg-opacity-40">
+                    <div className="flex justify-between w-full">
+                        <div className="font-normal">
+                            <p className="text-sm">Abstract Painting</p>
+                            <p className="text-xs">245 likes - 35 shares</p>
+                        </div>
+                        <div className="flex items-center">
+                                <img src="/images/bookmark.svg" alt="boolmark" />
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Container
